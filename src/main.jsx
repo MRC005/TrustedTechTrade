f91ba1514eccf006c6488b2f80a021135d6eea32
MRC005 <roychoudhurymachum05@gmail.com>
2025-06-06 23:32:52 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
+
+
